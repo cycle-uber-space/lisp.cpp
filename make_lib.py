b4f13df601790419a911adc213e87042f8f158ad
Cycle <cycle@uber.space>
2021-05-03 15:10:44 +0200

    drop hard-coded input paths

--- make_lib.py
+++ make_lib.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+import sys, re
+
+g_match = None
+
+def re_match(pattern, text):
+    global g_match
+    g_match = re.match(pattern, text)
+    return g_match
+
+def re_group(number):
+    global g_match
+    return g_match.group(number)
 
 def load_text(path):
     with open(path, "rt") as f:
@@ -8,16 +20,32 @@ def save_text(path, text):
     with open(path, "wt") as f:
         f.write(text)
 
+def split_text(text):
+    return text.split("\n")
+
+def join_lines(lines):
+    return "\n".join(lines)
+
+def load_lines(path):
+    return split_text(load_text(path))
+
+def save_lines(path, lines):
+    save_text(join_lines(lines))
+
 def load_source(path):
     return f"\n#line 2 \"{path}\"" + load_text(path)
 
-def main():
+def main(args):
+    srcs = list(args)
+
     text = ""
     text += """
 #ifndef _LISP_HPP_
 #define _LISP_HPP_
 """
-    text += load_source("src/lisp.hpp")
+    for src in srcs:
+        if src.endswith(".hpp"):
+            text += load_source(src)
     text += """
 #endif /* _LISP_HPP_ */
 
@@ -26,7 +54,9 @@ def main():
 #ifndef _LISP_CPP_
 #define _LISP_CPP_
 """
-    text += load_source("src/lisp.cpp")
+    for src in srcs:
+        if src.endswith(".cpp"):
+            text += load_source(src)
     text += """
 #endif /* _LISP_CPP_ */
 
@@ -36,4 +66,4 @@ def main():
     save_text("lisp.hpp", text)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
