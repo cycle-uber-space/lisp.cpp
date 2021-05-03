4328dab8cbf247ecd877987c0ccd2e25547d92f8
Cycle <cycle@uber.space>
2021-05-03 14:52:47 +0200

    extend std lisp

--- std.lisp
+++ std.lisp
@@ -47,6 +47,27 @@
 (defmacro let (decls . body)
   `((lambda ,(map car decls) ,@body) ,@(map cadr decls)))
 
+(defmacro let* (decls . body)
+  (defun expand (decls)
+    (if decls
+        (list 'let (list (car decls))
+              (expand (cdr decls)))
+        (cons 'progn body)))
+  (expand decls))
+
+(defmacro cond clauses
+  (defun expand (clauses)
+    (if clauses
+        (let* ((clause (car clauses))
+               (others (cdr clauses))
+               (test (car clause))
+               (body (cdr clause))
+               (then (cons 'progn body)))
+          (if others
+              (list 'if test then (expand others))
+              (list 'if test then)))))
+  (expand clauses))
+
 (defmacro progn body
   `(let () ,@body))
 
